0d323e7d418313450ded73b3ef37743e52f3e182
Atharva262005 <atharvaralegankar2005@gmail.com>
2024-11-01 21:51:58 +0530

    backend changed

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 
 // Add CORS middleware
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://gdgocpdeacoe.vercel.app");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
